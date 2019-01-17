da24414cf138722a4f4918ca03220e60c3bbe6cd
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-17 12:10:15 -0500

    finish

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import React, { Component} from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Basic Webpack with React</h1>
+        <h1 className="title">Basic Webpack with React</h1>
       </div>
     );
   }
