258556566b42c19fafed5d95a9eb3d0ef7726f4f
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-24 01:33:41 -0500

    added heroku deploy command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PATH_DIR = process.env.NODE_ENV === 'production' ? 'build' : 'public';
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, `client/${PATH_DIR}`)));
@@ -9,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, `client/${PATH_DIR}/index.html`))
 });
 
-app.listen(3000, () => {
-  console.log('Listening on Port 3000');
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
   
 })
